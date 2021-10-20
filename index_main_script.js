4f72ea1c0750eb9255f7f42fdbcd0710c616729e
Damien Delbreil <damien@lescommis.com>
2021-10-20 18:52:38 +0200

    update

--- index_main_script.js
+++ index_main_script.js
@@ -1,24 +1,3 @@
-
-// ---
-// document fourni par defaut
-// console.log(document)
-// type_of_html_element
-// div
-// p
-// span
-// h1
-// ul
-// li
-// example de fonction que document contient
-// function createElement(type_of_html_element) {
-//     // type_of_html_element = div
-//     // document.body ...
-// }
-// ---
-
-
-console.log(document)
-
 var amazon_db_telephones = [
     {
         nom: 'DOOGEE S97 PRO',
@@ -27,136 +6,89 @@ var amazon_db_telephones = [
         img: './tel1.webp'
     },
     {
-        nom: '',
+        nom: 'SAMSUNG GALAXY A12',
         prix: 336,
         description: 'android14',
         img: './tel2.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 226,
-        description: 'android15',
-        img: './tel3.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 186,
-        description: 'android13',
-        img: './tel1.webp'
-    },
-    {
-        nom: '',
-        prix: 336,
-        description: 'android14',
-        img: './tel2.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 226,
-        description: 'android15',
-        img: './tel3.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 186,
-        description: 'android13',
-        img: './tel1.webp'
-    },
-    {
-        nom: '',
-        prix: 336,
-        description: 'android14',
-        img: './tel2.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 226,
-        description: 'android15',
-        img: './tel3.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 186,
-        description: 'android13',
-        img: './tel1.webp'
-    },
-    {
-        nom: '',
-        prix: 336,
-        description: 'android14',
-        img: './tel2.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 226,
-        description: 'android15',
-        img: './tel3.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 186,
-        description: 'android13',
-        img: './tel1.webp'
-    },
-    {
-        nom: '',
-        prix: 336,
-        description: 'android14',
-        img: './tel2.webp'
-    },
-    {
-        nom: 'DOOGEE S97 PRO',
-        prix: 226,
-        description: 'android15',
-        img: './tel3.webp'
     },
 ]
 
 
-amazon_db_telephones.forEach(tel => {
-    // display objet var
-    // console.log(tel)
-    // console.log(tel.nom)
-    // console.log(tel.prix)
-    // console.log(tel.description)
-    // console.log(tel.img)
 
-    // je cre depuis le javascript tous les elements html necessaires pour reproduire le template de html de la carte du telephone
-    var div_amazon_tel = document.createElement('div')
-    // <div></div>
-    div_amazon_tel.style = "border: 1px solid black; width: 10vw;"
-    // <div style="border: 1px solid black; width: 10vw;"></div>
-    div_amazon_tel.classList.add("amazon_tel_card_template")
-    // <div class="amazon_tel_card_template"></div>
 
-    //
-    var img_amazon_tel = document.createElement('img')
-    img_amazon_tel.src = tel.img// https://blabla/tel2.jpg
-    console.log(img_amazon_tel)
-    console.dir(img_amazon_tel)
-    console.log(img_amazon_tel.baseURI)
-    // console.log(img_amazon_tel.height)
-    // img_amazon_tel.width = '300px'
-    img_amazon_tel.style = 'width: 100%;'
+function build(){
+    var ulPhoneList = document.createElement('ul')
+    ulPhoneList.classList.add("uk-grid-small")
+    ulPhoneList.classList.add("uk-child-width-1-2")
+    ulPhoneList.classList.add("uk-child-width-1-4@s")
+    ulPhoneList.id = "commeTuVeux"
+    // ulPhoneList.style = 'display: inline-block;'
+    ulPhoneList.setAttribute('uk-sortable', 'handle: .uk-sortable-handle')
+    ulPhoneList.setAttribute('uk-grid','')
 
-    var br_jump_1 = document.createElement('br')
+    amazon_db_telephones.forEach(tel => {
+        creation(tel, ulPhoneList)
+    })
+    document.body.appendChild(ulPhoneList)
+}
 
-    var a_amazon_tel_price = document.createElement('a')
-    a_amazon_tel_price.innerText = tel.prix
-    // console.dir(a_amazon_tel_price)
 
-    var br_jump_2 = document.createElement('br')
 
-    var a_amazon_tel_description = document.createElement('a')
-    a_amazon_tel_description.innerText = tel.description
-    a_amazon_tel_description.style = 'font-size: 10px;'
 
-    div_amazon_tel.appendChild(img_amazon_tel)
-    div_amazon_tel.appendChild(br_jump_1)
-    div_amazon_tel.appendChild(a_amazon_tel_price)
-    div_amazon_tel.appendChild(br_jump_2)
-    div_amazon_tel.appendChild(a_amazon_tel_description)
+function creation(tel, ulPhoneList){
+    var liPhoneCard = document.createElement('li')
 
-    document.body.appendChild(div_amazon_tel)
+    var divPhoneCard = document.createElement('div')
+    divPhoneCard.classList.add("uk-card")
+    divPhoneCard.classList.add("uk-card-default")
+    divPhoneCard.classList.add("uk-card-body")
+    divPhoneCard.classList.add("uk-text-center")
+    divPhoneCard.classList.add("uk-sortable-handle")
 
-})
+    var imgPhoneCard = document.createElement('img')
+    imgPhoneCard.src = tel.img
+
+    var nomPhoneCard = document.createElement('p')
+    nomPhoneCard.innerText = tel.nom
+
+    var prixPhoneCard = document.createElement('p')
+    prixPhoneCard.innerText = tel.prix
+
+    var descriptionPhoneCard = document.createElement('p')
+    descriptionPhoneCard.innerText = tel.description
+
+    divPhoneCard.appendChild(imgPhoneCard)
+    divPhoneCard.appendChild(nomPhoneCard)
+    divPhoneCard.appendChild(prixPhoneCard)
+    divPhoneCard.appendChild(descriptionPhoneCard)
+
+    liPhoneCard.appendChild(divPhoneCard)
+
+    ulPhoneList.appendChild(liPhoneCard)
+}
+
+
+
+function addPhone() {
+    var getModel = document.getElementById('inputModel').value
+    var getPrice = document.getElementById('inputPrice').value
+    var getDescription = document.getElementById('inputDescription').value
+    var getImg = document.getElementById('inputImg').value
+    var bt = document.getElementById('addPhoneButton')
+    var obj = {
+        nom: getModel,
+        prix: getPrice,
+        description: getDescription,
+        img: getImg
+    }
+    amazon_db_telephones.push(obj)
+    var ul = document.getElementById('commeTuVeux')
+    creation(obj, ul)
+    console.log(amazon_db_telephones.length)
+    // 'nom: ${getModel.value}', 'prix:${getPrice.value}', 'description:${getDescription.value}', 'img:${getImg.value}'
+    // bt.setAttribute("style", `grid-row: ${row};`)
+}
+
+
+
+build()
